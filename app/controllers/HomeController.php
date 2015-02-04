b790fc557519d003d67d030736db1948e1fbb39b
sam_tunebox <sam.wong@caterhammedia.com>
2015-02-04 18:15:48 +0800

    error message display hand picking

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -37,6 +37,50 @@ class HomeController extends BaseController {
 	public function doLogin()
 	{
 		// process the form
+		
+		// validate the info, create rules for the inputs
+		$rules = array(
+			'username'    => 'required', // make sure the email is an actual email
+			'password' => 'required' // password can only be alphanumeric and has to be greater than 3 characters
+		);
+    
+	
+		// run the validation rules on the inputs from the form
+		$validator = Validator::make(Input::all(), $rules);
+		
+		
+		
+		if ($validator->fails()) {
+			return Redirect::to('login')
+				->withErrors($validator) // send back all errors to the login form
+				->withInput(Input::except('password')); // send back the input (not the password) so that we can repopulate the form
+			
+			exit;	
+		}
+ 
+		// create our user data for the authentication
+		$userdata = array(
+			'username'     => Input::get('username'),
+			'password'  => Input::get('password')
+		);
+	
+		// attempt to do the login
+		//if (Auth::attempt($userdata)) 
+		if (Input::get('username') == 'SamWong') {
+	
+			// validation successful!
+			// redirect them to the secure section or whatever
+			// return Redirect::to('secure');
+			// for now we'll just echo success (even though echoing in a controller is bad)
+			echo 'SUCCESS!';
+	
+		} else {        
+	
+			// validation not successful, send back to form 
+			return Redirect::to('login');
+	
+		}
+ 
 	}
 
 }
--- app/views/home/login.blade.php
+++ app/views/home/login.blade.php
@@ -6,21 +6,29 @@
 <div class="row centered-form">
   <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
 	<div class="panel panel-default">
+	
+	  
+
 	  <div class="panel-heading">
 		<h3 class="panel-title">eDocCloud - Please Login</h3>
 	  </div>
 	  <div class="panel-body">
+
 		{{ Form::open(array('url' => 'login')) }}
 
-		  <div class="form-group">
-		   {{ Form::text('username', null, array('class'=>'form-control input','placeholder'=>'Username')) }}
-		  </div>
+		<div class="form-group @if ($errors->has('username')) has-error @endif">
+			{{ Form::text('username', null, array('class'=>'form-control input','placeholder'=>'Username')) }}
+		   
+			@if ($errors->has('username')) <p class="help-block">{{ $errors->first('username') }}</p> @endif
+		   
+		</div>
 
-		  <div class="form-group">
-		   {{ Form::password('password', array('class'=>'form-control input','placeholder'=>'Password')) }}
-		  </div>
+		<div class="form-group @if ($errors->has('password')) has-error @endif">
+			{{ Form::password('password', array('class'=>'form-control input','placeholder'=>'Password')) }}
+			@if ($errors->has('password')) <p class="help-block">{{ $errors->first('password') }}</p> @endif
+		</div>
            
-		   {{ Form::submit('Login', array('class'=>'btn btn-info btn-block')) }}
+			{{ Form::submit('Login', array('class'=>'btn btn-info btn-block')) }}
 
 		{{ Form::close() }}
 	  </div>
