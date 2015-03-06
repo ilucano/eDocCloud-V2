6cee61169083e49fe0bd9b80ad3c091d7fc89c3f
sam_tunebox <sam.wong@caterhammedia.com>
2015-03-07 00:15:18 +0800

    reassign REMOTE_ADDR value if detected cloudflare ip

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Cloudflare IP detection
+ */
+
+if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+  $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+}
+ 
 /*
 |--------------------------------------------------------------------------
 | Create The Application
