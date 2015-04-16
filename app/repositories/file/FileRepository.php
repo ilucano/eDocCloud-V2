c73c2be4ccb6d0191890d97b327ec35b57e4394d
sam_tunebox <sam.wong@caterhammedia.com>
2015-04-16 21:49:54 +0800

    File Repo and Interface

---
+++ app/repositories/file/FileRepository.php
@@ -0,0 +1,9 @@
+<?php namespace Repositories\File;
+
+use FileTable;
+use Company;
+
+class FileRepository implements FileRepositoryInterface
+{
+   
+}
---
+++ app/repositories/file/FileRepositoryInterface.php
@@ -0,0 +1,7 @@
+<?php
+namespace Repositories\File;
+ 
+interface FileRepositoryInterface
+{
+
+}
