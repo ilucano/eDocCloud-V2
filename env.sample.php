d90f5eb6fd53b5a347065a0b00fd72294ebb29f6
sam_tunebox <sam.wong@caterhammedia.com>
2015-06-10 22:20:13 +0800

    added sample .env file

---
+++ env.sample.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+
+    'LOGIN_APP_DOMAIN' => 'http://loginlocal.edoccloud.com',
+    'DB_HOST'      => 'localhost',
+    'DB_DATABASE'  => 'local_edoccloud',
+    'DB_USERNAME'  => 'root',
+    'DB_PASSWORD'  => '',
+    'DEBUG_MODE'   => true,
+    'APP_URL'      => 'http://local.edoccloud.com',
+);
